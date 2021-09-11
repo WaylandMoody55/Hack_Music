17a28b6ba5266b9029d79d1085b3cffbcfd50e5c
DonCarlosCreate <carlosareyna2020@gmail.com>
2021-09-11 17:40:24 -0500

    Initial commit

--- app/src/main/java/com/example/hack_music/MainActivity.java
+++ app/src/main/java/com/example/hack_music/MainActivity.java
@@ -5,13 +5,24 @@ package com.example.hack_music;
 /** hello my name is wayland*/
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button2 = findViewById(R.id.button2);
+        final MediaPlayer mediaPlayer = MediaPlayer.create( this, R.raw.piano);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
     }
 }
