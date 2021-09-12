cdc1c7d0e86cd647d8aadcd86bd8937134237cac
DonCarlosCreate <carlosareyna2020@gmail.com>
2021-09-12 00:56:05 -0500

    Initial commit

--- app/src/main/java/com/example/hack_music/MainActivity.java
+++ app/src/main/java/com/example/hack_music/MainActivity.java
@@ -5,24 +5,39 @@ package com.example.hack_music;
 /** hello my name is wayland*/
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button G3;
+    private Button button2;
+    private Button trumpet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        G3 = findViewById(R.id.G3);
+        button2 = findViewById(R.id.button2);
+        trumpet = findViewById(R.id.trumpet);
         final MediaPlayer mediaPlayer = MediaPlayer.create( this, R.raw.piano);
-        G3.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mediaPlayer.start();
             }
         });
+        trumpet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openTrumpet();
+            }
+        });
+
+    }
+
+    private void openTrumpet() {
+        Intent intent = new Intent(this,trumpet.class);
+        startActivity(intent);
     }
 }
