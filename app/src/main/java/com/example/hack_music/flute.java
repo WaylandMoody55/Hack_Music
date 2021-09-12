b4236de9d5b3c4563bc87ccf0dfa9d221724c6d2
DonCarlosCreate <carlosareyna2020@gmail.com>
2021-09-12 03:20:44 -0500

    Initial commit

--- app/src/main/java/com/example/hack_music/flute.java
+++ app/src/main/java/com/example/hack_music/flute.java
@@ -12,29 +12,51 @@ import android.view.View;
 import android.widget.Button;
 
 public class flute extends AppCompatActivity {
-    private Button button2;
     private Button trumpet;
     private Button piano;
-    private Button flute;
+    private Button violin;
+
+    private Button C5;
+    private Button C6;
+    private Button C4;
+    private Button G4;
+    private Button G5;
+    private Button G6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flute);
-        button2 = findViewById(R.id.button2);
+
         trumpet = findViewById(R.id.trumpet);
         piano = findViewById(R.id.piano);
-        flute = findViewById(R.id.flute);
-        final MediaPlayer mediaPlayer = MediaPlayer.create( this, R.raw.piano);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.start();
-            }
-        });
+        violin = findViewById(R.id.violin);
+
+        C5 = findViewById(R.id.C5);
+        C6 = findViewById(R.id.C6);
+        C4 = findViewById(R.id.C4);
+        G4 = findViewById(R.id.G4);
+        G5 = findViewById(R.id.G5);
+        G6 = findViewById(R.id.G6);
+
+        final MediaPlayer mediaPlayerG3 = MediaPlayer.create( this, R.raw.piano);
+        final MediaPlayer mediaPlayerC5 = MediaPlayer.create( this, R.raw.flute_c5);
+        final MediaPlayer mediaPlayerC6 = MediaPlayer.create( this, R.raw.flute_c6);
+        final MediaPlayer mediaPlayerC4 = MediaPlayer.create( this, R.raw.flute_c4);
+        final MediaPlayer mediaPlayerG4 = MediaPlayer.create( this, R.raw.flute_g4);
+        final MediaPlayer mediaPlayerG5 = MediaPlayer.create( this, R.raw.flute_g5);
+        final MediaPlayer mediaPlayerG6 = MediaPlayer.create( this, R.raw.flute_g6);
+
+
         trumpet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openTrumpet();
+            }
+        });
+        violin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openViolin();
             }
         });
         piano.setOnClickListener(new View.OnClickListener() {
@@ -43,21 +65,59 @@ public class flute extends AppCompatActivity {
                 openPiano();
             }
         });
-        flute.setOnClickListener(new View.OnClickListener() {
+
+        C5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openFlute();
+                mediaPlayerC5.seekTo(0);
+                mediaPlayerC5.start();
             }
         });
+        C6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerC6.seekTo(0);
+                mediaPlayerC6.start();
+            }
+        });
+        C4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerC4.seekTo(0);
+                mediaPlayerC4.start();
+            }
+        });
+        G4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerG4.seekTo(0);
+                mediaPlayerG4.start();
+            }
+        });
+        G5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerG5.seekTo(0);
+                mediaPlayerG5.start();
+            }
+        });
+        G6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerG6.seekTo(0);
+                mediaPlayerG6.start();
+            }
+        });
+
     }
 
-    private void openFlute() {
+    private void openPiano() {
         Intent intent = new Intent(this,flute.class);
         startActivity(intent);
     }
 
-    private void openPiano() {
-        Intent intent = new Intent(this,MainActivity.class);
+    private void openViolin() {
+        Intent intent = new Intent(this,violin.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/hack_music/violin.java
+++ app/src/main/java/com/example/hack_music/violin.java
@@ -12,35 +12,45 @@ import android.view.View;
 import android.widget.Button;
 
 public class violin extends AppCompatActivity {
-    private Button button2;
     private Button trumpet;
     private Button piano;
     private Button flute;
+
+    private Button C5;
+    private Button C6;
+    private Button C4;
+    private Button G4;
+    private Button G5;
+    private Button G6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_violin);
-        button2 = findViewById(R.id.button2);
+
         trumpet = findViewById(R.id.trumpet);
         piano = findViewById(R.id.piano);
         flute = findViewById(R.id.flute);
-        final MediaPlayer mediaPlayer = MediaPlayer.create( this, R.raw.piano);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.start();
-            }
-        });
+
+        C5 = findViewById(R.id.C5);
+        C6 = findViewById(R.id.C6);
+        C4 = findViewById(R.id.C4);
+        G4 = findViewById(R.id.G4);
+        G5 = findViewById(R.id.G5);
+        G6 = findViewById(R.id.G6);
+
+
+        final MediaPlayer mediaPlayerC5 = MediaPlayer.create( this, R.raw.violin_c5);
+        final MediaPlayer mediaPlayerC6 = MediaPlayer.create( this, R.raw.violin_c6);
+        final MediaPlayer mediaPlayerC4 = MediaPlayer.create( this, R.raw.violin_c4);
+        final MediaPlayer mediaPlayerG4 = MediaPlayer.create( this, R.raw.violin_g4);
+        final MediaPlayer mediaPlayerG5 = MediaPlayer.create( this, R.raw.violin_g5);
+        final MediaPlayer mediaPlayerG6 = MediaPlayer.create( this, R.raw.violin_g6);
+
+
         trumpet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openTrumpet();
-            }
-        });
-        piano.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openPiano();
             }
         });
         flute.setOnClickListener(new View.OnClickListener() {
@@ -49,15 +59,65 @@ public class violin extends AppCompatActivity {
                 openFlute();
             }
         });
-    }
+        piano.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPiano();
+            }
+        });
 
-    private void openFlute() {
-        Intent intent = new Intent(this,flute.class);
-        startActivity(intent);
+        C5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerC5.seekTo(0);
+                mediaPlayerC5.start();
+            }
+        });
+        C6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerC6.seekTo(0);
+                mediaPlayerC6.start();
+            }
+        });
+        C4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerC4.seekTo(0);
+                mediaPlayerC4.start();
+            }
+        });
+        G4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerG4.seekTo(0);
+                mediaPlayerG4.start();
+            }
+        });
+        G5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerG5.seekTo(0);
+                mediaPlayerG5.start();
+            }
+        });
+        G6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayerG6.seekTo(0);
+                mediaPlayerG6.start();
+            }
+        });
+
     }
 
     private void openPiano() {
         Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void openFlute() {
+        Intent intent = new Intent(this,flute.class);
         startActivity(intent);
     }
 
